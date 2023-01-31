849b7be7bb43ebeee82fc19291e42e475f09095e
alex <alexexander433223@gmail.com>
2023-01-31 22:23:26 +0100

    primer commit

--- pages/index.js
+++ pages/index.js
@@ -20,23 +20,6 @@ export default function Home() {
             AleZz
             <code className={styles.code}> - Portfolio</code>
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
         </div>
 
         <div className={styles.center}>
