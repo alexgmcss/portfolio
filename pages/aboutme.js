39e2d23e6249d54070afed24beda3c7b27342ab1
alex <alexexander433223@gmail.com>
2023-02-02 03:17:23 +0100

    primer commit

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import alezzImage from './alezz.jpg'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/AboutMe.module.css'
 
@@ -15,15 +15,15 @@ export default function AboutMe() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div>
-          <h1 class="aboutme">About Me</h1>
+      <p class="aboutme">About Me</p>
+        <div class="contenedor">
+        <div class="left-side">
+      <p class="name">Alex</p>
+      <p class="description">My name is Alex and I'm from Spain, in 2020 I founded my own company, a small independent record label. I consider myself passionate about what I do and very dedicated to my projects. </p>
         </div>
-
-        <div>
-        <img src="../IMG/alezz.jpg" alt="foto" class="foto" />
-          <p class="parrafo">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis libero libero. Integer id laoreet nisl. Nam dictum nisl quis est dapibus, a mollis libero bibendum.
-          </p>
+        <div class="right-side">
+            <img src={alezzImage} class="photo" alt="Foto" />
+        </div>
         </div>
       </main>
     </>
