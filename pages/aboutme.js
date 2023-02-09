97f0ac91f0347781f58a8701b40df660d4472bf0
alex <alexexander433223@gmail.com>
2023-02-09 01:01:20 +0100

    primer commit

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -10,8 +10,10 @@ const AboutMe = () => {
   <span></span>
   <span></span> Back Home
 </button>
-      <h1 className={styles.sobremi}>About Me</h1>
-      <p className={styles.bio}>Test</p>
+      <h1 className={styles.sobremi}>About<span className={styles.gradient}> Me</span></h1>
+      <p className={styles.bio}>Hi, Im Alex! Im a Junior Software Developer with a passion for creating innovative solutions to complex problems. Currently, I spend most of my time working on my personal projects, but Im actively seeking employment opportunities in software development.
+
+I am dedicated to my craft and constantly seeking to improve my skills and knowledge. With my passion for coding and my experience, I believe that I would be a great asset to any team.</p>
     </div>
   );
 };
