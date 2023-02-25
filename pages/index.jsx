570325b787525bb7bd61a1430857d2a81e83737c
alex <alexexander433223@gmail.com>
2023-02-25 14:00:45 +0100

    primer commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -30,7 +30,6 @@ export default function Home() {
           <a
             href="./aboutme"
             className={styles.card}
-            target="_blank"
             rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
